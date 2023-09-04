9ac89f12b3ff01bae33a99847f0a666b37b27050
caiogmello <caiogomedemello@gmail.com>
2023-09-04 19:14:45 -0300

    faltando q9, q11, q15 e q16

--- lista1.pl
+++ lista1.pl
@@ -1,5 +1,14 @@
 % Lista 1 - Caio Gomes de Mello
 
+% extra 
+
+membro(X, [X|_]) .
+membro(X, [_|R]) :- membro(X,R) .
+
+intersecao([], L, []) .
+intersecao([X|R1], L, [X|R2]) :- membro(X,L), intersecao(R1, L, R2), !.
+intersecao([_,R1], L, R2) :- intersecao(R1,L,R2).
+
 % q1 - concatenar
 
 concatenar([], L2, L2).
@@ -14,3 +23,66 @@ concatenarInv(L1, [X|R],[X|L3]) :- concatenarInv(L1,R,L3).
 
 concatenarL([], []).
 concatenarL([L|R], X) :- concatenarL(R, RX), concatenar(L,RX,X). 
+
+% q4 - mesclar 
+
+mesclando([], L2, L2).
+mesclando(L1, [], L1).
+mesclando([X|R1], [Y|R2], [X,Y|L3]) :- mesclando(R1,R2,L3).
+
+% q5 - adicionar no final
+
+adicionarFinal(A, [], [A]).
+adicionarFinal(A, [X|R1], [X|R2]) :- adicionarFinal(A,R1,R2).
+
+%  q6 - inverter uma lista
+
+inverter([],[]).
+inverter([X|R1], L2) :- inverter(R1,L), adicionarFinal(X,L,L2). 
+
+% q7 - inverter uma lista, tal que as sublistas tb sejam invertidas
+
+inverterLG([],[]).
+inverterLG([X|R1], [X2|L2]) :-  is_list(X),
+                                inverterLG(X, X2), inverterLG(R1,L2).
+inverterLG([X|R1], L2) :- inverterLG(R1,L), adicionarFinal(X,L,L2).
+
+% q8 - parear
+
+parear(E, [], []).
+parear(E, [X|R], [[E,X]|L]) :-  parear(E,R,L).
+
+% q9 - pares ERRADA
+
+pares([], []).
+pares([X|R], L) :- pares1(X,R,R2), pares(R,R2).
+
+pares1(_, [], []).
+pares1(X, [Y|R], [[X,Y]|R2]) :- pares1(X,R,R2).
+
+% q11 - permutar
+
+% q12 - conjunto
+
+conjunto([A]).
+conjunto([X|R]) :- \+ membro(X,R), conjunto(R).
+
+% q13 - prefixo
+
+prefixo([],_).
+prefixo([X|R1],[Y|R2]) :- X == Y, prefixo(R1,R2).
+
+%  q14 - subsequencia
+
+subsequencia([],_).
+subsequencia([X|R1],[Y|R2]) :- X \== Y, subsequencia([X|R1],R2).
+subsequencia([X|R1], [Y|R2]) :- X == Y, prefixo(R1,R2),!.
+
+% q15 - mescle ERRADA
+
+mescle([],X,X).
+mescle(X,[],X).
+mescle([X|R1],[Y|R2], [Y,X|R3]) :- X >= Y, mescle(R1,R2,R3).
+mescle([X|R1],[Y|R2], [X,Y|R3]) :- X < Y, mescle(R1,R2,R3).
+
+% q16 - mergeSort
